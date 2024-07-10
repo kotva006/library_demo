d8e4ac496eaffa5b713e33f259bd3cee79835654
kotva006 <ryan.kotval@gmail.com>
2024-07-10 08:14:18 -0500

    WIP adding the book creation flow

--- lib/library_demo_web/controllers/book_controller.ex
+++ lib/library_demo_web/controllers/book_controller.ex
@@ -2,15 +2,27 @@ defmodule LibraryDemoWeb.BookController do
   use LibraryDemoWeb, :controller
 
   alias LibraryDemo.Book
+  alias LibraryDemo.Author
 
   def show(conn, _params) do
     #TODO implement
     conn
   end
 
+  def author_to_name_id(%{pen_name: pen_name, id: id}) do
+    {pen_name, id}
+  end
+
   def new(conn, _params) do
     changeset = Book.changeset(%Book{})
-    render(conn, :new, changeset: changeset)
+    authors = Author.list();
+
+    author_options = authors |> Enum.map(&author_to_name_id/1)
+
+    conn
+    |> assign(:changeset, changeset)
+    |> assign(:authors, author_options)
+    |> render(:new)
   end
 
   def create(conn, params) do
