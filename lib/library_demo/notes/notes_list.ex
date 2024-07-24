7c80f65f7cc616ed5deffc9eab51ecb310ad46a8
kotva006 <ryan.kotval@gmail.com>
2024-07-24 17:48:17 -0500

    Added a note edit feature

--- lib/library_demo/notes/notes_list.ex
+++ lib/library_demo/notes/notes_list.ex
@@ -8,18 +8,30 @@ defmodule LibraryDemo.Notes.NotesList do
 
   @impl true
   def init(_) do
-    {:ok, %{next_id: 1, notes: []}}
+    {:ok, %{next_id: 1, notes: %{}}}
   end
 
   @impl true
   def handle_call(:list, _from, state) do
-    {:reply, state.notes, state}
+    notes_list = Enum.map(Map.to_list(state.notes), fn {_key, note} -> note end)
+    {:reply, notes_list, state}
   end
 
   @impl true
-  def handle_call({:create, %{"title" => title, "description" => description}}, _from, state) do
+  def handle_call({:create, %{title: title, description: description}}, _from, state) do
     note = %{title: title, description: description, id: state.next_id, date_modified: ""}
-    {:reply, note, %{next_id: state.next_id + 1, notes: [note | state.notes]}}
+    {:reply, note, %{next_id: state.next_id + 1, notes: Map.put(state.notes, state.next_id, note)}}
+  end
+
+  @impl true
+  def handle_call({:update, %{id: id} = note}, _from, state) do
+    new_notes = Map.put(state.notes, id, note)
+    {:reply, note, %{state | notes: new_notes}}
+  end
+
+  def handle_call({:get, note_id}, _from, state) do
+    found_note = Map.get(state.notes, note_id)
+    {:reply, found_note, state}
   end
 
   # Client API
@@ -27,7 +39,15 @@ defmodule LibraryDemo.Notes.NotesList do
     GenServer.call(__MODULE__, {:create, note})
   end
 
+  def update(note) do
+    GenServer.call(__MODULE__, {:update, note})
+  end
+
   def list() do
     GenServer.call(__MODULE__, :list)
+  end
+
+  def get(note_id) do
+    GenServer.call(__MODULE__, {:get, note_id})
   end
 end
--- lib/library_demo_web/notes/notes_list.ex
+++ lib/library_demo_web/notes/notes_list.ex
@@ -1,8 +1,6 @@
 defmodule LibraryDemoWeb.Notes.NotesList do
   use LibraryDemoWeb, :live_view
 
-  alias LibraryDemo.Notes.Note
-
   @impl true
   def mount(_params, _session, socket) do
     notes = LibraryDemo.Notes.NotesList.list()
@@ -10,14 +8,38 @@ defmodule LibraryDemoWeb.Notes.NotesList do
   end
 
   @impl true
+  def handle_event("save", %{"note_id" => ""} = params, socket) do
+    note = params_to_note(params)
+    LibraryDemo.Notes.NotesList.create(note)
+    notes = LibraryDemo.Notes.NotesList.list()
+    {:noreply, assign(socket, notes: notes, note_form: to_form(%{}))}
+  end
+
   def handle_event("save", params, socket) do
-    LibraryDemo.Notes.NotesList.create(params)
+    note = params_to_note(params)
+    LibraryDemo.Notes.NotesList.update(note)
     notes = LibraryDemo.Notes.NotesList.list()
     {:noreply, assign(socket, notes: notes, note_form: to_form(%{}))}
   end
 
   @impl true
-  def handle_event("inc_temperature", _params, socket) do
-    {:noreply, update(socket, :temperature, &(&1 + 1))}
+  def handle_event("edit_note", %{"note_id" => note_id}, socket) do
+    {note_id, _} = Integer.parse(note_id)
+    note = LibraryDemo.Notes.NotesList.get(note_id)
+    case note do
+      nil -> {:noreply, socket}
+      _ ->
+        note = %{"note_id" => note.id, "title" => note.title, "description" => note.description}
+        {:noreply, update(socket, :note_form, fn _ -> to_form(note) end)}
+    end
+  end
+
+  def params_to_note( %{"note_id" => "", "description" => description, "title" => title}) do
+    %{title: title, description: description, date_modified: ""}
+  end
+
+  def params_to_note(%{"note_id" => note_id, "description" => description, "title" => title}) do
+    {note_id, _} = Integer.parse(note_id)
+    %{id: note_id, title: title, description: description, date_modified: ""}
   end
 end
